70a6dac92622731f2362797b92d061a215c7a22b
DevCollynx <clementcollins987@gmail.com>
2025-10-24 17:49:18 +0100

    Add new features

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// HomePage
 const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
@@ -13,3 +14,6 @@ if (close) {
     nav.classList.remove('active');
   });
 }
+
+// sProduct
+var MainImg = document.getElementById(""); 
