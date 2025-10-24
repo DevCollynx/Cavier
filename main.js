601d93553724e90587e9d6eb9a513f16a0b83859
DevCollynx <clementcollins987@gmail.com>
2025-10-24 20:17:57 +0100

    New features

--- main.js
+++ main.js
@@ -10,10 +10,27 @@ if (bar) {
 }
 
 if (close) {
-  close.addEventListener('click', () => {
-    nav.classList.remove('active');
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
   });
 }
 
-// sProduct
-var MainImg = document.getElementById(""); 
+// Single Product
+var MainImg = document.getElementById("MainImg");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function () {
+  MainImg.src = smallImg[0].src;
+};
+
+smallImg[1].onclick = function () {
+  MainImg.src = smallImg[1].src;
+};
+
+smallImg[2].onclick = function () {
+  MainImg.src = smallImg[2].src;
+};
+
+smallImg[3].onclick = function () {
+  MainImg.src = smallImg[3].src;
+};
